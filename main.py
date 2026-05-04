9afece2a6f379c217adfe9fd7caf3d7e93e1fe13
kaua <kaua7santos7oliveiraa@gmail.com>
2026-05-04 11:18:01 -0300

    .

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def cadastrar_usuario(
     senha: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    user_existente = db.query(Usuario).filter_by(email==email).first()
+    user_existente = db.query(Usuario).filter_by(email=email).first()
 
     if user_existente: 
         return templates.TemplateResponse(
@@ -52,3 +52,48 @@ def cadastrar_usuario(
 
     return RedirectResponse(url="/login", status_code=303)
 
+# função rota fazer login
+@app.post("/login")
+def fazer_login(
+    request: Request,
+    email: str = Form(...),
+    senha: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    # verificar usuario
+    user_existente = db.query(Usuario).filter(Usuario.email == email). filter(Usuario.senha == senha).first()
+
+    if user_existente is None:
+        return templates.TemplateResponse(
+            request,
+            "cadastro.html",
+            {"request": request, "erro": "Email ou senha ja cadastrado ou invalidos"}
+        )
+    
+    response = RedirectResponse(url="/home", status_code=302)
+
+    # criar um cookie simples
+    response.set_cookie(
+        key="usuario_id",
+        value=str(user_existente.id)
+    )
+    return response
+
+# criar a rota protegida
+@app.get("/home")
+def home(
+    request: Request,
+    db: Session = Depends(get_db)
+):
+    usuario_id = request.cookies.get("usuario_id")
+
+    if usuario_id is None:
+        return RedirectResponse(url="/login", status_code=303)
+    
+    usuario = db.query(Usuario).get(int(usuario_id))
+
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"request": request, "usuario": usuario}
+    )
